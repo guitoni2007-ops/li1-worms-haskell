497248b7d4f226b81f80c36847746c43edd77fde
dinissilva <a116004@alunos.uminho.pt>
2025-10-29 11:18:11 +0000

    Trabalho local antes do rebase

--- lib/Tarefa0_geral.hs
+++ lib/Tarefa0_geral.hs
@@ -35,33 +35,57 @@ data Direcao = Norte | Nordeste | Este | Sudeste | Sul | Sudoeste | Oeste | Noro
 
 -- | Verifica se o indice pertence à lista.
 eIndiceListaValido :: Int -> [a] -> Bool
-eIndiceListaValido = undefined
+eIndiceListaValido x l = x >= 0 && x < length l
+
 
 -- | Calcula a dimensão de uma matriz.
 --
 -- __NB:__ Note que não existem matrizes de dimensão /m * 0/ ou /0 * n/, e que qualquer matriz vazia deve ter dimensão /0 * 0/.
 dimensaoMatriz :: Matriz a -> Dimensao
-dimensaoMatriz = undefined
+dimensaoMatriz [] = (0, 0)
+dimensaoMatriz m  = (length m, length (head m))
 
 -- | Verifica se a posição pertence à matriz.
 ePosicaoMatrizValida :: Posicao -> Matriz a -> Bool 
-ePosicaoMatrizValida = undefined
+ePosicaoMatrizValida (l,c) m = eIndiceListaValido l m && eIndiceListaValido c (head m)
+
 
 -- | Move uma posição uma unidade no sentido de uma direção.
 movePosicao :: Direcao -> Posicao -> Posicao
-movePosicao = undefined
+movePosicao d (l,c)   | d==Norte = (l-1,c)
+                      | d==Sul = (l+1,c)
+                      | d==Oeste = (l,c-1)
+                      | d==Este = (l,c+1)
+                      | d==Nordeste = (l-1,c+1)
+                      | d==Noroeste = (l-1,c-1)
+                      | d==Sudeste = (l+1,c+1)
+                      | d==Sudoeste = (l+1,c-1)
+             
 
 -- | Versão da função 'movePosicao' que garante que o movimento não se desloca para fora de uma janela.
 --
 -- __NB:__ Considere uma janela retangular com origem no canto superior esquerdo definida como uma matriz. A função recebe a dimensao da janela.
 movePosicaoJanela :: Dimensao -> Direcao -> Posicao -> Posicao
-movePosicaoJanela = undefined
+movePosicaoJanela (x,y) d (l,c) | d==Norte && l > 0 = (l-1,c)
+                                | d==Sul && l < x-1 = (l+1,c)
+                                | d==Oeste && c > 0 = (l,c-1)
+                                | d==Este && c < y-1 = (l,c+1)
+                                | d==Nordeste && l > 0 && c < y-1 = (l-1,c+1)
+                                | d==Noroeste && l > 0 && c > 0 = (l-1,c-1)
+                                | d==Sudeste && l < x-1 && c < y-1 = (l+1,c+1)
+                                | d==Sudoeste && l < x-1 &&  c > 0 = (l+1,c-1)
+
 
 -- | Converte uma posição no referencial em que a origem é no canto superior esquerdo da janela numa posição em que a origem passa a estar no centro da janela.
 --
 -- __NB:__ Considere posições válidas. Efetue arredondamentos como achar necessário.
 origemAoCentro :: Dimensao -> Posicao -> Posicao
-origemAoCentro = undefined
+origemAoCentro (x,y) (l,c) =
+  let centroL = x `div` 2
+      centroC = y `div` 2
+  in (c - centroC, centroL - l)
+
+                        
 
 -- | Roda um par (posição,direção) 45% para a direita.
 --
@@ -69,7 +93,15 @@ origemAoCentro = undefined
 --
 -- <<https://haslab.github.io/Teaching/LI1/2526/img/rodaposicaodirecao.png>>
 rodaPosicaoDirecao :: (Posicao,Direcao) -> (Posicao,Direcao)
-rodaPosicaoDirecao = undefined
+rodaPosicaoDirecao ((l,c),d) | d==Norte = ((l,c), Nordeste)
+                             | d==Nordeste = ((l,c), Este)
+                             | d==Este = ((l,c), Sudeste)
+                             | d==Sudeste = ((l,c), Sul)
+                             | d==Sul = ((l,c), Sudoeste)
+                             | d==Sudoeste = ((l,c), Oeste)
+                             | d==Oeste = ((l,c), Noroeste)
+                             | d==Noroeste = ((l,c), Norte)
+                            
 
 -- * Funções recursivas.
 
@@ -77,36 +109,56 @@ rodaPosicaoDirecao = undefined
 --
 -- __NB:__ Retorna @Nothing@ se o índice não existir.
 encontraIndiceLista :: Int -> [a] -> Maybe a
-encontraIndiceLista = undefined
+encontraIndiceLista _ [] = Nothing
+encontraIndiceLista i (h:t) | i<0 = Nothing
+                            | i==0 = Just h
+                            | otherwise = encontraIndiceLista (i-1) t
 
 -- | Modifica um elemento num dado índice.
 --
--- __NB:__ Devolve a própria lista se o elemento não existir.
+-- __NB:__ Devolve a própria lista se o índice não existir.
 atualizaIndiceLista :: Int -> a -> [a] -> [a]
-atualizaIndiceLista = undefined
+atualizaIndiceLista _ _ [] = []
+atualizaIndiceLista i x l@(h:t)
+  | i < 0     = l                    
+  | i == 0    = x : t                 
+  | otherwise = h : atualizaIndiceLista (i - 1) x t  
+
 
 -- | Devolve o elemento numa dada posição de uma matriz.
 --
 -- __NB:__ Retorna @Nothing@ se a posição não existir.
 encontraPosicaoMatriz :: Posicao -> Matriz a -> Maybe a
-encontraPosicaoMatriz = undefined
+encontraPosicaoMatriz _ [] = Nothing                                  
+encontraPosicaoMatriz (0, c) (linha : _) = encontraIndiceLista c linha  
+encontraPosicaoMatriz (l, c) (_ : resto) = encontraPosicaoMatriz (l - 1, c) resto
 
 -- | Modifica um elemento numa dada posição de uma matriz.
 --
 -- __NB:__ Devolve a própria matriz se o elemento não existir.
 atualizaPosicaoMatriz :: Posicao -> a -> Matriz a -> Matriz a
-atualizaPosicaoMatriz = undefined
+atualizaPosicaoMatriz _ _ [] = []
+atualizaPosicaoMatriz (0, c) e (linha:t) = atualizaIndiceLista c e linha : t
+atualizaPosicaoMatriz (l, c) e (linha:t) = linha : atualizaPosicaoMatriz (l - 1, c) e t
+
 
 -- | Aplica uma sequência de movimentações a uma posição, pela ordem em que ocorrem na lista.
 moveDirecoesPosicao :: [Direcao] -> Posicao -> Posicao
-moveDirecoesPosicao = undefined
+moveDirecoesPosicao [] pos = pos                             
+moveDirecoesPosicao (h:t) pos = moveDirecoesPosicao t (movePosicao h pos)
+
 
 -- | Aplica a mesma movimentação a uma lista de posições.
 moveDirecaoPosicoes :: Direcao -> [Posicao] -> [Posicao]
-moveDirecaoPosicoes = undefined
+moveDirecaoPosicoes _ [] = []                      
+moveDirecaoPosicoes d (h:t) = movePosicao d h : moveDirecaoPosicoes d t
+
 
 -- | Verifica se uma matriz é válida, no sentido em que modela um rectângulo.
---
--- __NB:__ Todas as linhas devem ter o mesmo número de colunas. 
+-- __NB:__ Todas as linhas devem ter o mesmo número de colunas.
 eMatrizValida :: Matriz a -> Bool
-eMatrizValida = undefined
+eMatrizValida [] = True
+eMatrizValida [_] = True
+eMatrizValida (l1:l2:ls)
+  | length l1 == length l2 = eMatrizValida (l2:ls)
+  | otherwise = False
--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -6,10 +6,7 @@ Módulo para a realização da Tarefa 1 de LI1\/LP1 em 2025\/26.
 -}
 module Tarefa1 where
 
-import Labs2025
+import Labs2025 
 
 -- | Função principal da Tarefa 1. Recebe um estado e retorna se este é válido ou não.
 validaEstado :: Estado -> Bool
-validaEstado e = undefined
-
-
