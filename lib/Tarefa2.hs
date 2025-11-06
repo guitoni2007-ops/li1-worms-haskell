f56bc5f3535be989e40d2d5331ff63dbf8654005
dinissilva <a116004@alunos.uminho.pt>
2025-11-06 17:44:49 +0000

    Tarefa2Disparos

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -10,6 +10,7 @@ module Tarefa2 where
 import Labs2025
 import Tarefa0_2025
 -- | Função principal da Tarefa 2. Recebe o índice de uma minhoca na lista de minhocas, uma jogada, um estado e retorna um novo estado em que essa minhoca efetuou essa jogada.
+
 efetuaJogada :: NumMinhoca -> Jogada -> Estado -> Estado
 efetuaJogada n j e = undefined
 
@@ -23,7 +24,9 @@ validaminhocaviva = all estaViva
 jogadaMoveLivre :: Estado -> Posicao -> Jogada -> Bool
 jogadaMoveLivre estado pos (Move dir) =
   let destino = movePosicao dir pos
-  in ePosicaoEstadoLivre destino estado
+      mapa = mapaEstado estado
+  in ePosicaoMatrizValida destino mapa && ePosicaoEstadoLivre destino estado
+
 
 estaNoChao :: Estado -> Minhoca -> Bool
 estaNoChao estado minhoca =
@@ -42,24 +45,60 @@ podeEfetuarJogada _ _ _ = True
 podeMoverMinhoca :: Estado -> Minhoca -> Jogada -> Bool
 podeMoverMinhoca estado minhoca (Move _) = estaNoChao estado minhoca
 
+matarMinhoca :: Minhoca -> Minhoca
+matarMinhoca m = m { posicaoMinhoca = Nothing, vidaMinhoca = Morta }
+
 mortePorForaMapa :: Estado -> Minhoca -> Jogada -> Minhoca
 mortePorForaMapa estado minhoca (Move dir) =
   case posicaoMinhoca minhoca of
-    Nothing -> minhoca  -- já está sem posição
     Just pos ->
       let destino = movePosicao dir pos
           mapa = mapaEstado estado
       in if ePosicaoMatrizValida destino mapa
-         then minhoca  -- posição válida, continua viva
-         else minhoca { posicaoMinhoca = Nothing, vidaMinhoca = Morta }
-mortePorForaMapa _ minhoca _ = minhoca  -- só interessa a jogada Move
+         then minhoca
+         else matarMinhoca minhoca
+    Nothing -> minhoca
+mortePorForaMapa _ minhoca _ = minhoca
+
+mortePorAgua :: Estado -> Minhoca -> Jogada -> Minhoca
+mortePorAgua estado minhoca (Move dir) =
+  case posicaoMinhoca minhoca of
+    Just pos ->
+      let destino = movePosicao dir pos
+      in if ePosicaoMatrizValida destino (mapaEstado estado)
+         then case encontraPosicaoMatriz destino (mapaEstado estado) of
+                Just Agua -> minhoca { posicaoMinhoca = Just destino, vidaMinhoca = Morta }
+                _         -> minhoca
+         else minhoca  -- posição fora do mapa → não faz nada ou já morre noutro lugar
+    Nothing -> minhoca
+
+verificaJogadaMov :: Estado -> Minhoca -> Jogada -> Minhoca
+verificaJogadaMov estado minhoca jogada@(Move dir)
+  | not (podeEfetuarJogada estado minhoca jogada) = minhoca
+  | not (podeMoverMinhoca estado minhoca jogada)  = minhoca
+  | otherwise =
+      let minhoca1 = mortePorForaMapa estado minhoca jogada
+          minhoca2 = mortePorAgua estado minhoca1 jogada
+          posValida = case posicaoMinhoca minhoca2 of
+                        Just pos -> jogadaMoveLivre estado pos jogada
+                        Nothing  -> False
+      in if posValida then minhoca2 else minhoca2
+verificaJogadaMov _ minhoca _ = minhoca
+
+----------------------------Disparos------------------------------------------------------------------
+-- | Verifica se a minhoca pode disparar a arma e atualiza a munição caso possa.
+
+verificaJogadaDisparo :: Minhoca -> TipoArma -> Maybe Minhoca
+verificaJogadaDisparo minhoca arma
+  | vidaMinhoca minhoca == Morta = Nothing  -- não pode disparar se estiver morta
+  | encontraQuantidadeArmaMinhoca arma minhoca <= 0 = Nothing  -- sem munição
+  | otherwise = Just (disparaArma arma minhoca)  -- decrementa a munição
 
 
 
+  
 
-
-
-
+  
 
 
 
