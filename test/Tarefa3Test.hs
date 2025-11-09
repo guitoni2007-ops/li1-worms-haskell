4398ccecb7d5864011bc8bdf4700fd26c8bb519e
Guilherme Martins <a114978@alunos.uminho.pt>
2025-11-09 23:39:37 +0000

    Tarefa3 feita com testes feitos

--- test/Tarefa3Test.hs
+++ test/Tarefa3Test.hs
@@ -10,7 +10,7 @@ cabal run --enable-coverage t3-feedback
 -}
 -- | Definir aqui os testes do grupo para a Tarefa 3
 testesTarefa3 :: [Estado]
-testesTarefa3 = [e1,e2,e3,e4,e5]
+testesTarefa3 = [e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13]
 
 dataTarefa3 :: IO TaskData
 dataTarefa3 = do
@@ -28,12 +28,15 @@ e1 = Estado mapaTesteValido1 [] minhocasTeste1
 e2 = Estado mapaTesteValido2 [] minhocasTeste1
 e3 = Estado mapaTesteValido3 [] minhocasTeste1
 e4 = Estado mapaTesteValido4 [] minhocasTeste2
-e5 = Estado mapaTesteValido1 objetosTeste1 []
-
-
-
-
-
+e5 = Estado mapaTesteValido1 objetosTeste1 minhocasTeste1
+e6 = Estado mapaTesteValido5 objetosTeste2 []
+e7 = Estado mapaTesteValido5 objetosTeste3 []
+e8 = Estado mapaTesteValido5 objetosTeste4 []
+e9 = Estado mapaTesteValido5 objetosTeste5 []
+e10 = Estado mapaTesteValido5 objetosTeste6 []
+e11 = Estado mapaTesteValido6 objetosTeste7 []
+e12 = Estado mapaTesteValido6 objetosTeste8 []
+e13 = Estado mapaTesteValido5 objetosTeste7 minhocasTeste3
 
 
 
@@ -78,6 +81,20 @@ mapaTesteValido4 =
   , [Ar, Pedra, Terra, Agua]
   ]
 
+mapaTesteValido5 =
+  [ [Ar, Ar, Ar, Ar, Ar]
+  , [Ar, Ar, Ar, Ar, Ar]
+  , [Ar, Ar, Ar, Ar, Ar]
+  , [Ar, Ar, Ar, Ar, Ar]
+  , [Ar, Ar, Ar, Ar, Ar]
+  ]
+mapaTesteValido6 =
+  [ [Ar, Ar, Ar, Ar, Ar]
+  , [Ar, Ar, Ar, Ar, Ar]
+  , [Ar, Ar, Terra, Ar, Ar]
+  , [Ar, Ar, Ar, Ar, Ar]
+  , [Ar, Ar, Ar, Ar, Ar]
+  ]
 
 
 minhocasTeste1 =
@@ -86,6 +103,9 @@ minhocasTeste1 =
 minhocasTeste2 =
   [ Minhoca (Just (3,0)) (Viva 100) 1 1 1 1 1 ]
 
+minhocasTeste3 =
+  [ Minhoca (Just (2,2)) (Viva 100) 1 1 1 1 1 ]
+
 
 
 
@@ -93,3 +113,29 @@ minhocasTeste2 =
 objetosTeste1 =
   [ Barril (2,2) True ]
  
+objetosTeste2 =
+  [ Barril (2,2) False ]
+ 
+objetosTeste3 =
+  [ Disparo (3,3) Norte Bazuca Nothing 5 ]
+
+objetosTeste4 =
+  [ Disparo (0,0) Norte Bazuca Nothing 5 ]
+
+objetosTeste5 =
+  [ Disparo (1,2) Norte Dinamite Nothing 0
+  , Disparo (2,3) Nordeste Dinamite Nothing 0
+  , Disparo (2,4) Sudoeste Dinamite Nothing 0
+  , Disparo (3,3) Noroeste Dinamite Nothing 0
+  , Disparo (1,1) Sudeste Dinamite Nothing 0
+  , Disparo (3,2) Sul Dinamite Nothing 0
+  ]
+
+objetosTeste6 =
+  [ Disparo (3,3) Sul Mina Nothing 5 ]
+
+objetosTeste7 =
+  [ Disparo (2,2) Sul Mina Nothing 5 ]
+
+objetosTeste8 =
+  [ Disparo (3,3) Norte Escavadora (Just 2) 5 ]
