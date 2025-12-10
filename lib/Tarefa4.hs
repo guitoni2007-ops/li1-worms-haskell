fd30f5f859c4e8c358ed68d2e42c59856622e6e0
dinissilva <a116004@alunos.uminho.pt>
2025-12-10 12:35:22 +0000

    Tarefa4Botinccompleto

--- lib/Tarefa4.hs
+++ lib/Tarefa4.hs
@@ -43,5 +43,36 @@ avancaObjetoJogada e objetos (i,objeto') = if elem objeto' objetos
     else Left objeto'
 
 -- | Para um número de ticks desde o início da tática, dado um estado, determina a próxima jogada.
-jogadaTatica :: Ticks -> Estado -> (NumMinhoca,Jogada)
-jogadaTatica t e = undefined
+jogadaTatica :: Ticks -> Estado -> (NumMinhoca, Jogada)
+jogadaTatica t e =
+    let minhocas = minhocasEstado e
+        -- Seleciona a minhoca que vai jogar (ciclo pelas minhocas disponíveis)
+        minhocaAtual = minhocas !! (t `mod` length minhocas)
+        Just (x, y) = posicaoMinhoca minhocaAtual
+        -- Verifica se há minhocas adversárias no alcance
+        minhocasAdversarias = filter (\m -> posicaoMinhoca m /= posicaoMinhoca minhocaAtual) minhocas
+        -- Verifica se há barris no alcance
+        barris = filter (\(Barril pos _) -> pos == (x + 1, y) || pos == (x - 1, y) || pos == (x, y + 1) || pos == (x, y - 1)) (objetosEstado e)
+    in
+        if not (null minhocasAdversarias) then
+            -- Se houver minhocas adversárias, ataca a mais próxima
+            let Just (ax, ay) = posicaoMinhoca (head minhocasAdversarias)
+            in if ax > x then (0, Move Sul) -- Move para baixo
+               else if ax < x then (0, Move Norte) -- Move para cima
+               else if ay > y then (0, Move Este) -- Move para a direita
+               else if ay < y then (0, Move Oeste) -- Move para a esquerda
+               else (0, Dispara Bazuca Norte) -- Ataca com a bazuca
+        else if not (null barris) then
+            -- Se não houver minhocas, mas houver barris, ataca o barril mais próximo
+            let (bx, by) = posicaoBarril (head barris)
+            in if bx > x then (0, Move Sul)
+               else if bx < x then (0, Move Norte)
+               else if by > y then (0, Move Este)
+               else if by < y then (0, Move Oeste)
+               else (0, Dispara Bazuca Norte)
+        else
+            -- Caso contrário, move-se para explorar o mapa
+            if t `mod` 4 == 0 then (0, Move Norte)
+            else if t `mod` 4 == 1 then (0, Move Este)
+            else if t `mod` 4 == 2 then (0, Move Sul)
+            else (0, Move Oeste)
