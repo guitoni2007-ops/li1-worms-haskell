f85aaf49ef4d651f77967ed45bab3e403d55ccfd
dinissilva <a116004@alunos.uminho.pt>
2025-12-23 16:49:44 +0000

    Tarefa4feita

--- lib/Tarefa4.hs
+++ lib/Tarefa4.hs
@@ -7,7 +7,7 @@ Módulo para a realização da Tarefa 4 de LI1\/LP1 em 2025\/26.
 module Tarefa4 where
 
 import Data.Either
-
+import Data.Maybe (fromMaybe)
 import Labs2025
 import Tarefa2
 import Tarefa3
@@ -43,36 +43,55 @@ avancaObjetoJogada e objetos (i,objeto') = if elem objeto' objetos
     else Left objeto'
 
 -- | Para um número de ticks desde o início da tática, dado um estado, determina a próxima jogada.
+-- | Para um número de ticks desde o início da tática, dado um estado, determina a próxima jogada.
+-- | Para um número de ticks desde o início da tática, dado um estado, determina a próxima jogada.
 jogadaTatica :: Ticks -> Estado -> (NumMinhoca, Jogada)
-jogadaTatica t e =
-    let minhocas = minhocasEstado e
-        -- Seleciona a minhoca que vai jogar (ciclo pelas minhocas disponíveis)
-        minhocaAtual = minhocas !! (t `mod` length minhocas)
-        Just (x, y) = posicaoMinhoca minhocaAtual
-        -- Verifica se há minhocas adversárias no alcance
-        minhocasAdversarias = filter (\m -> posicaoMinhoca m /= posicaoMinhoca minhocaAtual) minhocas
-        -- Verifica se há barris no alcance
-        barris = filter (\(Barril pos _) -> pos == (x + 1, y) || pos == (x - 1, y) || pos == (x, y + 1) || pos == (x, y - 1)) (objetosEstado e)
-    in
-        if not (null minhocasAdversarias) then
-            -- Se houver minhocas adversárias, ataca a mais próxima
-            let Just (ax, ay) = posicaoMinhoca (head minhocasAdversarias)
-            in if ax > x then (0, Move Sul) -- Move para baixo
-               else if ax < x then (0, Move Norte) -- Move para cima
-               else if ay > y then (0, Move Este) -- Move para a direita
-               else if ay < y then (0, Move Oeste) -- Move para a esquerda
-               else (0, Dispara Bazuca Norte) -- Ataca com a bazuca
-        else if not (null barris) then
-            -- Se não houver minhocas, mas houver barris, ataca o barril mais próximo
-            let (bx, by) = posicaoBarril (head barris)
-            in if bx > x then (0, Move Sul)
-               else if bx < x then (0, Move Norte)
-               else if by > y then (0, Move Este)
-               else if by < y then (0, Move Oeste)
-               else (0, Dispara Bazuca Norte)
-        else
-            -- Caso contrário, move-se para explorar o mapa
-            if t `mod` 4 == 0 then (0, Move Norte)
-            else if t `mod` 4 == 1 then (0, Move Este)
-            else if t `mod` 4 == 2 then (0, Move Sul)
-            else (0, Move Oeste)
+jogadaTatica t e = (idx, acao)
+  where
+    minhocas = minhocasEstado e
+    -- 1. Seleciona a minhoca viva (ciclo)
+    vivas = [i | (i, m) <- zip [0..] minhocas, vidaMinhoca m /= Morta]
+    idx = if null vivas then 0 else vivas !! (t `mod` length vivas)
+    mAtual = minhocas !! idx
+    
+    -- 2. Define a ação baseada na posição atual
+    acao = case posicaoMinhoca mAtual of
+        Nothing -> Move Norte
+        Just (x, y) -> decidir (x, y)
+    
+    decidir (x, y)
+        -- 1. Atacar: Só se houver inimigo VIVO alinhado
+        | not (null alvosVivos) = Dispara Bazuca (dirPara (head alvosVivos) (x, y))
+        
+        -- 2. Escavar: Se houver Terra adjacente
+        | not (null terras) = Dispara Escavadora (head terras)
+        
+        -- 3. Perseguir: Se houver inimigos vivos noutro lado
+        | not (null inimigosVivos) = Move (dirPara (posicaoInimigo (head inimigosVivos)) (x, y))
+        
+        -- 4. EXPLORAR: Caso não haja nada acima, volta ao movimento aleatório
+        | otherwise = [Move Norte, Move Este, Move Sul, Move Oeste] !! (t `mod` 4)
+      where
+        -- Filtra inimigos que estão na mesma linha/coluna e que estão VIVOS
+        alvosVivos = [p | m <- inimigosVivos, 
+                         let p = posicaoInimigo m, 
+                         vidaMinhoca m /= Morta,
+                         let (ax, ay) = p, ax == x || ay == y]
+
+        terras = [d | d <- [Norte, Este, Sul, Oeste], 
+                      let pDest = movePosicao d (x, y),
+                      case encontraPosicaoMatriz pDest (mapaEstado e) of
+                          Just Terra -> True
+                          _ -> False]
+
+    -- Funções auxiliares globais ao jogadaTatica
+    inimigosVivos = [m | (i, m) <- zip [0..] minhocas, i /= idx, vidaMinhoca m /= Morta]
+    
+    posicaoInimigo m = fromMaybe (0,0) (posicaoMinhoca m)
+
+    dirPara (ax, ay) (cx, cy)
+        | ax < cx = Norte
+        | ax > cx = Sul
+        | ay > cy = Este
+        | ay < cy = Oeste
+        | otherwise = Norte
--- test/Tarefa4Test.hs
+++ test/Tarefa4Test.hs
@@ -3,10 +3,10 @@ module Main where
 import Labs2025
 import Tarefa4
 import Magic
-
+---cabal clean && rm -rf t4-feedback.tix
 -- | Definir aqui os testes do grupo para a Tarefa 4
 testesTarefa4 :: [Estado]
-testesTarefa4 = []
+testesTarefa4 = [e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,e16,e17,e18,e19]
 
 dataTarefa4 :: IO TaskData
 dataTarefa4 = do
@@ -16,3 +16,120 @@ dataTarefa4 = do
 
 main :: IO ()
 main = runFeedback =<< dataTarefa4
+
+
+e1 = Estado mapaTesteValido1 [] minhocasTeste1
+e2 = Estado mapaTesteValido2 [] minhocasTeste1 
+e3 = Estado mapaTesteValido1 [] minhocasTeste1
+e4 = Estado mapaTesteValido2 [] minhocasTeste2
+e5 = Estado mapaTesteValido1 objetosTeste1 minhocasTeste1
+e6 = Estado mapaTesteValido1 objetosTeste2 minhocasTeste2
+e7 = Estado mapaTesteValido1 objetosTeste3 minhocasTeste3
+e8 = Estado mapaTesteValido2 objetosTeste4 minhocasTeste1
+e9 = Estado mapaTesteValido1 objetosTeste5 minhocasTeste2
+e10 = Estado mapaTesteValido1 objetosTeste6 minhocasTeste2
+e11 = Estado mapaTesteValido1 objetosTeste7 minhocasTeste3
+e12 = Estado mapaTesteValido2 objetosTeste1 minhocasTeste1
+e13 = Estado mapaTesteValido1 objetosTeste7 minhocasTeste3
+
+e14 = Estado mapaTesteValido1 [] minhocasDuelo
+e15 = Estado mapaTesteValido3 [] minhocasDuelo2
+
+e16 = Estado mapaTesteValido3 objetosTeste11 minhocasDuelo2
+
+e17 = Estado mapaTesteValido2 [] minhocasTeste4
+
+e18 = Estado mapaTesteValido4 [] minhocasTeste3
+
+e19 = Estado mapaTesteValido3 objetosTeste11 minhocasDuelo3
+-- Mapas válido
+mapaTesteValido1 =
+  [ [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Agua,Agua,Agua]
+  , [Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Agua,Agua,Agua]
+  , [Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Agua,Agua,Agua]
+  ]
+
+mapaTesteValido2 =
+  [ [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Pedra,Terra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Terra,Agua,Agua,Agua]
+  , [Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Agua,Agua,Agua]
+  , [Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Agua,Agua,Agua]
+  ]
+
+mapaTesteValido3 =
+  [ [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Terra, Ar,Ar, Ar,Ar, Ar]
+  , [Pedra,Terra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Terra,Agua,Agua,Agua]
+  , [Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Agua,Agua,Agua]
+  , [Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Agua,Agua,Agua]
+  ]
+
+mapaTesteValido4 =
+  [ [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar,Ar, Ar]
+  , [Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Terra,Agua,Agua,Agua]
+  , [Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Terra,Agua,Agua,Agua]
+  , [Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Pedra,Agua,Agua,Agua]
+  ]
+minhocasTeste1 =
+  [ Minhoca (Just (2,8)) (Viva 100) 1 15 5 5 5 ]
+
+minhocasTeste2 =
+  [ Minhoca (Just (2,2)) (Viva 100) 1 15 5 5 5 ]
+
+minhocasTeste3 =
+  [ Minhoca (Just (2,4)) (Viva 100) 1 14 5 5 5 ]
+
+minhocasTeste4 =
+  [ Minhoca (Just (2,4)) (Viva 100) 1 0 0 0 0 ]
+objetosTeste1 =
+  [ Barril (2,10) False ]
+ 
+objetosTeste2 =
+  [ Barril (2,10) False ]
+ 
+objetosTeste3 =
+  [ Barril (2,10) False ]
+
+objetosTeste11 =
+  [ Barril (2,4) False ]
+objetosTeste4 =
+  [ Disparo (2,6) Norte Mina Nothing 5 ]
+
+objetosTeste5 =
+  [ Disparo (2,2) Norte Dinamite Nothing 0
+  , Disparo (2,3) Nordeste Dinamite Nothing 0
+  , Disparo (2,4) Sudoeste Dinamite Nothing 0
+  , Disparo (2,3) Noroeste Dinamite Nothing 0
+  , Disparo (2,1) Sudeste Dinamite Nothing 0
+  , Disparo (2,9) Sul Dinamite Nothing 0
+  ]
+
+objetosTeste6 =
+  [ Disparo (2,3) Sul Mina Nothing 5 ]
+
+objetosTeste7 =
+  [ Disparo (2,2) Sul Mina Nothing 5 ]
+
+minhocasDuelo =
+  [ Minhoca (Just (2,2)) (Viva 100) 1 15 5 5 5  -- Minhoca 0
+  , Minhoca (Just (2,5)) (Viva 100) 1 15 5 5 5  -- Minhoca 1
+  ]
+
+minhocasDuelo2 =
+  [ Minhoca (Just (2,2)) (Viva 10) 1 15 5 5 5  -- Minhoca 0
+  , Minhoca (Just (2,5)) (Viva 100) 1 15 5 5 5  -- Minhoca 1
+  , Minhoca (Just (1,8)) (Viva 30) 1 15 5 5 5
+  ]
+
+minhocasDuelo3 =
+  [ Minhoca (Just (2,2)) (Viva 100) 1 15 5 5 5  -- Minhoca 0
+  , Minhoca (Just (1,8)) (Viva 100) 1 15 5 5 5  -- Minhoca 1
+  ]
